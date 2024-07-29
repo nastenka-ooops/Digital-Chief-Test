8cf4d09697e4e07f1a0a1c52e7339512912db625
nastenka-ooops <nastenka.brutskaya@mail.ru>
2024-07-29 10:20:11 +0300

    init commit

---
+++ src/main/java/com/example/digitalChiefTest/DigitalChiefTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.digitalChiefTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalChiefTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalChiefTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/digitalChiefTest/DigitalChiefTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitalChiefTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalChiefTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
